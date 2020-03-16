36c83dd054067b15be45539e4bfac6c9b2136590
unknown <jazzbasskdg@gmail.com>
2020-03-16 16:43:21 +0900

    Coin Change, Maximum Subarray

---
+++ src/myStudy/algorithms/dp/CoinChange.java
@@ -0,0 +1,93 @@
+package myStudy.algorithms.dp;
+
+import java.util.Arrays;
+
+//O(N * S) , S=amount
+public class CoinChange {
+	public static int INF = Integer.MAX_VALUE / 2;
+	
+	public static int coinChange(int[] coins, int amount) {
+		
+		int N = coins.length;
+		
+		int[][] DP = new int[N+1][amount+1];
+		
+		Arrays.fill(DP[0], INF);
+		DP[0][0] = 0;
+		
+		//per coin
+		for(int i=1; i<=N; i++) {
+			
+			int coinValue = coins[i-1];
+
+			//per value, set coin count
+			for(int j=1; j<=amount; j++) {
+				
+				DP[i][j] = DP[i-1][j];
+				
+				if( j - coinValue >= 0 && DP[i][j-coinValue] + 1 < DP[i-1][j] ) {
+					DP[i][j] = DP[i][j-coinValue] + 1;
+				}
+			}
+		}		
+		for(int i = 0; i<DP.length; i++) {
+			System.out.println(Arrays.toString(DP[i]));
+		}
+		if(DP[N][amount] == INF) return -1;
+
+
+		return DP[N][amount];
+	}
+	
+	// Space Complexity : O(n*S)
+	public static int coinChangeSpaceEfficient ( int[] coins, int amount) {
+		
+		int N = coins.length;
+		int[] DP = new int[amount+1];
+		Arrays.fill(DP, INF);
+		DP[0] = 0;
+		
+		for(int coinValue : coins) {
+			
+			for(int j=1; j<=amount; j++) {
+				if( j-coinValue >=0 && DP[j-coinValue] + 1 < DP[j])
+					DP[j] = DP[j-coinValue] + 1;
+			}
+		}
+		
+		if(DP[amount] == INF) return -1;
+		return DP[amount];
+	}
+	
+	public static int coinChangeRecursive(int[] coins, int amount) {
+		
+		
+		int[] DP = new int[amount + 1];
+
+		return coinChangeRecursive(coins, amount, DP);
+	}
+	
+	private static int coinChangeRecursive(int[] coins, int amount, int[] DP) {
+		
+	    // Base cases.
+	    if (amount < 0) return -1;
+	    if (amount == 0) return 0;
+	    if (DP[amount] != 0) return DP[amount];
+	    
+	    int minValue = INF;
+	    for(int coinValue : coins) {
+	    	
+	    	int newAmount = amount - coinValue;
+	    	int value = coinChangeRecursive(coins, newAmount, DP);	
+	    	if(value != -1 && value < minValue) minValue = value + 1;
+	    }
+	    
+	    return (minValue != INF) ? DP[amount] = minValue : -1;
+	}
+	public static void main(String[] args) {
+		int[] coins = {2,5,1};
+		System.out.println(coinChange(coins,11));
+		System.out.println(coinChangeSpaceEfficient(coins,11));
+		System.out.println(coinChangeRecursive(coins,11));
+	}
+}
---
+++ src/myStudy/algorithms/leetcode/MaximumSubarray.java
@@ -0,0 +1,50 @@
+package myStudy.algorithms.leetcode;
+
+public class MaximumSubarray {
+
+	//O(n^2)
+	public static int maximumSubarrayValueNaive(int[] ar) {
+		
+		int n = ar.length;
+		
+		int max = Integer.MIN_VALUE/2;
+		//길이
+		for(int i=0; i<n; i++) {
+
+			//시작점
+			for(int k=0; k+i<n; k++) {
+				int sum=0;	
+				//합
+				for(int j=k; j <=k+i ; j++) sum += ar[j];
+			
+				max = Math.max(max, sum);
+			}
+		}
+		return max;
+	}
+	
+	//O(n)...!!
+	public static int maximumSubarrayValue(int[] ar) {
+		int n = ar.length;
+		
+		int maxVal, sum;
+		maxVal = sum = ar[0];
+		
+		for(int i=1; i<n; i++) {
+			
+			// if sum이 +라면 : sum += ar[i], sum이 -라면 sum = ar[i]
+			if( ar[i] < sum + ar[i]) sum += ar[i];
+			else sum = ar[i];
+			maxVal = Math.max(sum, maxVal);
+		}
+		
+		return maxVal;
+	}
+	
+	public static void main(String[] args) {
+		// ans : -5, -1, 41
+		 System.out.println(maximumSubarrayValue(new int[] {-5}));
+		 System.out.println(maximumSubarrayValue(new int[] {-5, -4, -10, -3, -1, -12, -6}));
+		 System.out.println(maximumSubarrayValue(new int[] {1, 2, 1, -7, 2, -1, 40, -89}));		
+	}
+}
