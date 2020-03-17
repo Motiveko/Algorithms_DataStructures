0c50d5c2896a7864c901d2542b3437ec15dc5e2f
unknown <jazzbasskdg@gmail.com>
2020-03-17 16:08:00 +0900

    maxDistToClosest 까지

---
+++ src/myStudy/algorithms/algorithms/FirstUniqChar.java
@@ -0,0 +1,44 @@
+package myStudy.algorithms.algorithms;
+
+import java.util.*;
+
+//Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+public class FirstUniqChar {
+	
+	public static int solve1(String s) {
+		if(s ==null || s.length() ==0) return -1;
+		int n = s.length();
+		Map<Character, Integer> map = new HashMap<>();
+		
+		for(int i=0; i<n; i++) {
+			
+			char val = s.charAt(i);
+			if(map.containsKey(val)) map.put(val, 2);
+			else map.put(val, 1);
+		}
+		
+		for(int i=0; i<n; i++) 
+			if(map.get(s.charAt(i)) == 1) return i;
+		
+		return -1;
+	}
+	
+	//String의 기본 메소드를 이용한 방법, 간단하나 느리다고 한다.
+	public static int solve2(String s) {
+		if(s ==null || s.length() ==0) return -1;
+		int n = s.length();
+		
+		for(int i=0; i<n; i++) 
+			if( s.indexOf(s.charAt(i)) == s.lastIndexOf(s.charAt(i))) 
+				return i;
+		
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(solve1("@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxy"));
+		System.out.println(solve2("@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxy"));
+
+	}
+}
---
+++ src/myStudy/algorithms/algorithms/MaxDistToClosest.java
@@ -0,0 +1,70 @@
+package myStudy.algorithms.algorithms;
+
+import java.util.Random;
+
+/*
+In a row of seats, 1 represents a person sitting in that seat, 
+and 0 represents that the seat is empty. 
+There is at least one empty seat, and at least one person sitting.
+Alex wants to sit in the seat such that the distance between him 
+and the closest person to him is maximized. 
+Return that maximum distance to closest person.
+Example 1:
+Input: [1,0,0,0,1,0,1]
+Output: 2
+Explanation: 
+If Alex sits in the second open seat (seats[2]), then the closest person has distance 2.
+If Alex sits in any other open seat, the closest person has distance 1.
+Thus, the maximum distance to the closest person is 2.
+Example 2:
+Input: [1,0,0,0]
+Output: 3
+Explanation: 
+If Alex sits in the last seat, the closest person is 3 seats away.
+This is the maximum distance possible, so the answer is 3.
+Note:
+1 <= seats.length <= 20000
+seats contains only 0s or 1s, at least one 0, and at least one 1.
+ */
+public class MaxDistToClosest {
+
+	
+	public static int maxDistToClosest(int[] seats) {
+		
+		int n = seats.length;
+		int flag = 0;
+		
+		int maxDist=0;
+		int curDist=0;
+		for(int i=0; i<n; i++) {
+			
+			if(seats[i] ==1) {
+				//시작이 0이면서 첫 flag
+				if(seats[0]==0 && flag == 0) {
+					maxDist = i+1;
+					flag = i;
+				}
+				curDist = (i-flag)/2;
+				maxDist = Math.max(maxDist, curDist);
+				flag = i;
+			}
+			
+			//마지막 값이 0일 경우 길이계산
+			if( i==n-1 && seats[i]==0) {
+				curDist = i-flag;
+				maxDist = Math.max(maxDist, curDist);
+			}
+		}
+		
+		return maxDist;
+	}
+	
+	public static void main(String[] args) {
+		int n = 1000;
+		int[] seats = new int[n];
+		Random ran = new Random();
+		for(int i=0; i<n; i++) seats[i] = ran.nextInt(2); 
+		
+		System.out.println(maxDistToClosest(seats));
+	}
+}
