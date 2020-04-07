ce67d466bdd0c5a5c52a126379e3c764dd4521b3
unknown <jazzbasskdg@gmail.com>
2020-04-07 16:43:43 +0900

    sgt minqueryrange 구현하면댐

---
+++ src/myStudy/datastructure/segmenttree/SegmentTreeRMQ.java
@@ -0,0 +1,9 @@
+package myStudy.datastructure.segmenttree;
+
+//Range에서 최소값찾는거, FisetVersion에도 구현되어있다.
+//https://www.geeksforgeeks.org/segment-tree-set-1-range-minimum-query/?ref=lbp
+public class SegmentTreeRMQ {
+
+	
+	
+}
