493735f93e08e8c0f4a791ffeb0dff9cc4a1e217
unknown <rhehdrla@naver.com>
2020-05-11 17:15:26 +0900

    backpack 5 space efficiency

--- src/myStudy/algorithms/dp/Backpack5.java
+++ src/myStudy/algorithms/dp/Backpack5.java
@@ -13,6 +13,7 @@ import java.util.Arrays;
 public class Backpack5 {
 	
 	//결국 스스로 해내지 못했다.. 중복허용을 피하는 방법.. 매우 대단하다..
+	// Naive, Space efficiency O(n*m)
 	public static int solve1(int[] nums, int target) {
 		
 		if(nums==null || target==0) throw new IllegalArgumentException("invalid input");
@@ -31,9 +32,48 @@ public class Backpack5 {
 		for( int[] d : DP) System.out.println(Arrays.toString(d));
 		return DP[n][target];
 	}
+	
+	//Rolling array, space efficiency O(2*m), m==target
+	public static int solve2(int[] nums, int target) {
+		
+		int[][] DP = new int[2][target+1];
+		
+		DP[1][0]= 1;
+		
+		for( int i=0; i<nums.length; i++) {
+			int val = nums[i];
+			
+			for( int j=0; j<= target ; j++) {
+				DP[i%2][j] = DP[(i+1)%2 ][j];
+				if( j - val >= 0 ) DP[i%2][j] += DP[(i+1)%2][j-val];
+			}
+		}
+		
+		return DP[(nums.length-1)%2][target];
+	}
+	
+	// space efficiency O(m)
+	public static int solve3(int[] nums, int target) {
+		
+		int[] DP = new int[target+1];
+		
+		DP[0] = 1;
+		int maxVal = 0;
+		for( int i=0; i<nums.length; i++) {
+			int val = nums[i];
+			maxVal += val;
+			for( int j=0; j<=Math.min(maxVal, target); j++) {
+				if( j-val >= 0) DP[j] += DP[j-val];
+			}
+		}
+		
+		return DP[target];		
+	}
 
 	public static void main(String[] args) {
 
-		System.out.println(solve1(new int[] { 2, 3, 5, 7, 8 }, 10));
+		System.out.println(solve1(new int[] { 2, 3, 5, 7, 8 }, 115));
+		System.out.println(solve2(new int[] { 2, 3, 5, 7, 8 }, 15));
+		System.out.println(solve2(new int[] { 2, 3, 5, 7, 8 }, 15));
 	}
 }
