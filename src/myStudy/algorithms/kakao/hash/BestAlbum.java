4f4db4124b6132592232826ac9844910bb5bb918
MotiveKo <rhehdrla@naver.com>
2020-08-25 21:02:12 +0900

    카카오 해시 4번가지 풀이완

--- src/myStudy/algorithms/kakao/hash/BestAlbum.java
+++ src/myStudy/algorithms/kakao/hash/BestAlbum.java
@@ -1,84 +1,80 @@
 package myStudy.algorithms.kakao.hash;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 
 public class BestAlbum {
 	static class Solution {
 	    public static int[] solution(String[] genres, int[] plays) {
 	        
-	    	HashMap<String, Integer> totalMap = new HashMap<>(); // 장르 - 토탈실행
-	    	HashMap<String, ArrayList<Integer>>  genreMap = new HashMap<>(); // 장르, 장르별 id	
-	    	for(int i=0; i<genres.length; i++) {
+	    	// 필요한거
+	    	// 장르별 총 재생수 및 그에대한 순위
+	    	// 장르별 1,2위의 ID
+	    	HashMap<String, Integer> totalMap = new HashMap<>(); // 장르 - 총재생횟수
+	    	HashMap<Integer, String> reverseTotalMap = new HashMap<>(); // 총재생횟수 - 장르
+
+	    	// 각 재생횟수 바로 정렬때려서 eachMap에서 id를 가져온다.
+	    	HashMap<String, ArrayList<Integer>> genreMap = new HashMap<>(); // 장르, (재생횟수1,2,3..)
+	    	HashMap<Integer, ArrayList<Integer>> eachMap = new HashMap<>(); // 재생횟수 - id ( 중복따져야한다.)
+	    	
+	    	for( int i=0; i<genres.length; i++) {
 	    		
-	    		Integer totalPlay = totalMap.get(genres[i]);
-	    		if(totalPlay==null) {
-	    			totalMap.put(genres[i],plays[i]);
-	    			genreMap.put(genres[i], new ArrayList<>(Arrays.asList(i)));
-	    			
-	    		} else {
-	    			totalMap.put(genres[i], totalPlay + plays[i]);
-	    			genreMap.get(genres[i]).add(i);
-	    		}
+	    		String genre = genres[i];
+	    		int play = plays[i];
 	    		
+	    		totalMap.put(genre, totalMap.getOrDefault(genre, 0)+play);
+	    		if(!genreMap.containsKey(genre)) genreMap.put(genre, new ArrayList<>());
+	    		genreMap.get(genre).add(play);
+	    		
+	    		if(!eachMap.containsKey(play)) eachMap.put(play, new ArrayList<>());
+	    		eachMap.get(play).add(i);
 	    	}
-	    	// (total , 순위)		
-	    	String[] sortedGenres = new String[totalMap.size()];
-	    	ArrayList<Integer> genreRank = new ArrayList<>();
-	    	totalMap.values().stream().forEach( a -> genreRank.add(a));
-	    	genreRank.sort(null);;
-	    	for( int i=0; i<totalMap.size(); i++) {
-	    		Iterator<String> itr = totalMap.keySet().iterator();
-	    		while(itr.hasNext()) {
-	    			String genre = itr.next();
-	    			Integer genrePlay = totalMap.get(genre);
-	    			sortedGenres[genreRank.indexOf(genrePlay)] = genre; 
-	    		}
-	    	}
+	    	
+	    	ArrayList<Integer> sortedTotalPlays = new ArrayList<>();
+	    	
+	    	totalMap.values().stream().forEach( total -> sortedTotalPlays.add(total));
+	    	totalMap.keySet().stream().forEach( key -> reverseTotalMap.put(totalMap.get(key), key));
+	    	
+	    	Collections.sort(sortedTotalPlays, Collections.reverseOrder());	    	
 	    	
 	    	ArrayList<Integer> answer = new ArrayList<>();
-	    	for( String genre : sortedGenres) {
-	    		
-	    		ArrayList<Integer> list = genreMap.get(genre);
-	    		
-	    		Integer first = list.get(0);
-	    		if( list.size()==1) {
-	    			answer.add(first);
-	    			continue;
-	    		}
-	    		Integer second = list.get(1);
-	    		if( plays[first] < plays[second]) {
-	    			int tmp = second;
-	    			second = first;
-	    			first = tmp;
-	    		}
-	    		for( int i=2;i<list.size();i++ ) {
+	    	for( Integer totalPlay : sortedTotalPlays) {
+	    		// 장르 순위별로 꺼내보잣
+	    		String genre = reverseTotalMap.get(totalPlay); // 현재장르
+	    		ArrayList<Integer> list = genreMap.get(genre); // 해당장르 각 재생횟수
+	    		Collections.sort(list,Collections.reverseOrder()); // 해당장르 재생횟수 정렬
 
-	    			if( plays[i] > first) {
-	    				second = first;
-	    				first = list.get(i);
-	    			} else if( plays[i] > plays[second]) second=list.get(i);
+	    		// 장르 내에서 순서대로 꺼내보자
+	    		for( int i=0; i<2; i++) {
+	    			if( list.size() <= i) break;
+	    			Integer playTime = list.get(i);
+	    			
+	    			// 해당 재생횟수를 가지는 ids, 오름차순 정렬 	
+
+	    			ArrayList<Integer> IdsWithSamePlay = eachMap.get(playTime);
+	    			// 	Collections.sort(IdsWithSamePlay);	// 이미 넣을때부터 오름차순으로 넣는다.
+	    			for( Integer id : IdsWithSamePlay) {
+	    				if(genres[id].equals(genre)) {
+	    					// 이미 같은 재생횟수, 장르 를 가지는 아이디가 들어가있다!	
+	    					if(answer.contains(id)) continue;
+	    					answer.add(id);
+	    					break;
+	    				}
+	    			}
 	    		}
-	    		answer.add(second);
-	    		answer.add(first);	    		
 	    	}
-	    	
-	    	
 	    	int[] a = new int[answer.size()];
 	    	for( int i =0; i<a.length;i++) {
-	    		a[i] = answer.get(a.length-i-1);
+	    		a[i] = answer.get(i);
 	    	}
-	    	
 	    	return a;
 	    }
-
 	}
+	
 	public static void main(String[] args) {
-		
-		for( int x : Solution.solution(new String[] {"classic", "pop", "classic", "classic", "pop"},
-				new int[] {50,60,15,80,250})) {
+		for( int x : Solution.solution(new String[] {"classic", "pop"},
+											new int[] {50,60})) {
 			System.out.println(x + ",");
 		};
 	}	
