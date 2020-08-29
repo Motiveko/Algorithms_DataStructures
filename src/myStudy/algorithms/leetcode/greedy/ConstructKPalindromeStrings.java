d536d78d3085a07f03cbd660d01069fb8abac250
MotiveKo <rhehdrla@naver.com>
2020-08-30 02:15:04 +0900

    Greedy - ConstructKPalindromeStrings

--- src/myStudy/algorithms/leetcode/greedy/ConstructKPalindromeStrings.java
+++ src/myStudy/algorithms/leetcode/greedy/ConstructKPalindromeStrings.java
@@ -7,21 +7,32 @@ import java.util.*;
  */
 import java.util.HashMap;
 
+// https://leetcode.com/problems/construct-k-palindrome-strings/submissions/
+// input s에 대해서 k개의 palindrome을 만들수 있는지를 출력하는 문제
 public class ConstructKPalindromeStrings {
 	
+	// 나는 풀지 못하였습니다.. 
+	// 이 문제의 핵심은 같은문자가 짝수개와 홀수개 일 때 각각 가지는 특성을 파악하는것이다. 
+	// 머리를 더 쓰자!
 	public static boolean canConstruct(String s, int k) {
 		
-		int n = s.length();
-		if( n==k ) return true;
-		HashMap<String,Integer> map = new HashMap<>();
-		
-		for( int i=0 ;i<n; i++) map.put(s.substring(i,i+1), map.getOrDefault(s.substring(i,i+1),0)+1);
-		
-		int[] arr = map.values().stream().mapToInt(Integer::intValue).toArray();
-		Arrays.sort(arr); // 캐릭터 갯수 순서대로..
-		
-		
-		return false;
+        if (s.length() < k) {
+            return false;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        int count = 0;
+        for (char c : map.keySet()) {
+            if (map.get(c) % 2 == 1) {
+                count++;
+            }
+        }
+        return count <= k;   
 	}
 
+	public static void main(String[] args) {
+		System.out.println(canConstruct("bb",0));
+	}
 }
